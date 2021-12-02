b5579d03412f52539474e91e0d5a31667e8d2e4d
Andrew Schmidt (CHIP) <anschmidt@microsoft.com>
2021-12-01 18:38:01 -0600

    add support me button

--- components/Footer.js
+++ components/Footer.js
@@ -10,6 +10,7 @@ const Footer = (props) => (
                 <li>&copy; nemcrunchers.dev</li><li>Made with <a src="https://nextjs.org/">Next.js</a></li>
             </ul>
         </div>
+        <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Buy me a coffee', '#29abe0', 'N4N5784YU');kofiwidget2.draw();</script> 
     </footer>
 )
 
